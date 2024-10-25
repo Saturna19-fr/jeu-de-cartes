21a55e2b6aa284d223ee323aabb3711be74ac63c
Saturna19 <contact@saturna19.fr>
2024-10-25 16:01:11 +0200

    Add VSCode settings for Python unit testing and create test files
    - Added a new `.vscode/settings.json` file with configuration for running Python unit tests using the `unittest` framework.
    - Created a new `tests.md` file with a checklist for test scenarios.
    - Updated the `test_cartes.py` file to import classes from the `src` package and added test methods for checking card display and battle functionality.

---
+++ tests.md
@@ -0,0 +1,5 @@
+- [ ] Longueur du paquet si y'a toutes les cartes
+- [ ] S'il y a des dupliqués
+
+- [ ] Simuler une bataille avec x cartes
+
--- tests/test_cartes.py
+++ tests/test_cartes.py
@@ -1,24 +1,20 @@
 import unittest
-from classes import Carte
-
-class TestCartes(unittest.TestCase):
-
-    # Création de cartes
-    def setup(self):
-        self.carte = Carte("trèfle", 8)
+import src.classes
+class Test_TestIsValid(unittest.TestCase):
+    def setUp(self):
+        self.carte = src.classes.Carte("trèfle", 8)
     
-    def affiche_correctement(self):
-        self.assertIs(str(self.carte), "8 de trèfle")
+    def test_affiche_correctement(self):
+        self.assertEqual(str(self.carte), "8 de trèfle")
 
-    def bat_correctement(self):
-        carte_test = Carte("trèfle", 5)
-        carte_test2 = Carte("pic", 12)
-        carte_test3 = Carte("carreau", 14)
+    def test_bat_correctement(self):
+        carte_test = src.classes.Carte("trèfle", 5)
+        carte_test2 = src.classes.Carte("pic", 12)
+        carte_test3 = src.classes.Carte("carreau", 14)
 
         self.assertTrue(self.carte.bat(carte_test), "Le 8 doit battre le 5")
         self.assertFalse(self.carte.bat(carte_test2), "Le 12 doit battre le 8")
         self.assertFalse(self.carte.bat(carte_test3), "Le 14 doit battre le 8")
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
