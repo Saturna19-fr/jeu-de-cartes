61713dcbf133553a6d5923d3108dfef044d67ca6
Thibault Morez <contact@saturna19.fr>
2024-10-27 21:46:05 +0100

    Refactor code; Added repr on Carte, Deck to make it easier on print statements, Added a str func to show the content of the deck. Fixed the Joueur.ramasser method, and created a ramasser_all to claim all cards passed in args.
    Created the Bataille logic

--- src/classes.py
+++ src/classes.py
@@ -1,5 +1,7 @@
 from random import randint, shuffle
-
+import os
+import time
+import json
 CARTES = {
     14: "As",
     13: "Dame",
@@ -23,7 +25,8 @@ class Carte:
     def __init__(self, couleur:str, nombre:int) -> None:
         self.couleur = couleur
         self.nombre = nombre
-
+    def __repr__(self) -> str:
+        return self.__str__()
     def __str__(self) -> str:
         """ Carte -> str
         Méthode spéciale. Permet d'afficher la carte. """
@@ -65,11 +68,12 @@ class Deck:
         return len(self.contenu)
     
     def __sizeof__(self) -> int: return self.__len__()
-
+    def __repr__(self) -> str:
+        return self.__str__()
     def __str__(self) -> str:
         """Deck -> str
         Renvoie une chaine de caractère décrivant le deck. """
-        return ", ".join(map(str, self.contenu))
+        return ", ".join(map(str, self.contenu)) if self.contenu else "Deck vide."
     
 
     
@@ -97,78 +101,128 @@ class Joueur:
     def ramasser(self, carte1:Carte, carte2:Carte) -> None:
         """ Joueur, Carte, Carte -> Nonetype
             Ajoute les deux cartes en dessous de la main. """
-        return [carte1, carte2, *self.main]
+        self.main = [carte1, carte2, *self.main]
+    
+    def ramasser_all(self, *cartes):
+        self.main = [*cartes, *self.main]
     
 
-class Bataille:
-    """Simule le jeu de la bataille entre deux joueurs"""
-    def __init__(self, j1, j2):
-        """Bataille, Joueur, Joueur -> NoneType"""
+class Bataille():
+    def __init__(self, j1, j2) -> None:
         self.joueur1:Joueur = j1
         self.joueur2:Joueur = j2
-        self.pile = [] # Ca sera toujours 2 cartes, la première est celle du joueur 1, la deuxième celle du joueur 2.
+        self.pile = []
+
         self.deck = Deck()
-        self.deck.melanger() 
+        self.deck.melanger()
+        self.distribuer()
 
 
-    def distribuer(self):
+    def fin_partie(self, ind:int=0, getPlayer:bool = False) -> bool|Joueur:
+        """ Bataille, int -> bool
+        Vérifie si la partie est terminée. Si ind est passé,alors on vérifie si un des deux joueurs a ind cartes, sinon 0. """
+        print("Fin de partie: ", len(self.joueur1.main) == ind or len(self.joueur2.main) == ind)
+        checkFP = len(self.joueur1.main) == ind or len(self.joueur2.main) == ind
+        if getPlayer:
+            return self.joueur1 if len(self.joueur1.main) > len(self.joueur2.main) else self.joueur2
+        return checkFP
+
+    def jouer_tour(self, skipcheck = False) -> None:
+        if self.fin_partie():
+            if len(self.pile) > 0:
+                self.fin_partie(getPlayer=True).ramasser_all(*self.pile)
+                self.pile = []
+            return # Partie finie, même en cas de bataille !
+                
+        print("On commence un tour avec {}, {}".format(self.joueur1.main[-1], self.joueur2.main[-1]))
+        # On compare les deux cartes
+        print(len(self.joueur1.main), len(self.joueur2.main))
+        self.pile.append(self.joueur1.jouer_carte())
+        self.pile.append(self.joueur2.jouer_carte())
+        print(len(self.joueur1.main), len(self.joueur2.main))
+        print(len(self.pile))
+
+        carte_une_bat_deux = self.pile[-2].bat(self.pile[-1])
+        if skipcheck: return carte_une_bat_deux
+        # Si la carte une est égale à la carte deux, la fonction renvoie None. Donc, bataille.
+        if carte_une_bat_deux is None:
+            return self.bataille()
+
+        if carte_une_bat_deux:
+            print("Le joueur 1 gagne !")
+            self.joueur1.ramasser_all(*self.pile)
+            print(self.joueur1.main)
+            self.pile = []
+        else:
+            print("Le joueur 2 gagne !")
+            self.joueur2.ramasser_all(*self.pile)
+            print(self.joueur2.main)
+            self.pile = []
+
+# ToDo: ajouter un check pour vérifier si bataille à la fin + plus de cartes ne fait plus crash.
+# En cas de bataille, choisir entre réinjecter une carte random ou conclure sur la personne ayant le
+# plus de points
+
+
+    def bataille(self):
+        # Dans le cadre de la bataille:
+        self.jouer_tour(True)
+        self.jouer_tour()
+
+    def distribuer(self) -> None:
         """ Bataille -> NoneType
         Distribue le deck aux joueurs """
+        print("Mon deck est ", self.deck)
         for i in range(0, len(self.deck)):
             if i % 2 == 0:
                 self.joueur1.piocher_carte(self.deck)
             else:
                 self.joueur2.piocher_carte(self.deck)
-        print(self.deck)
+        # Tests de la partie bataille
+        # self.joueur1.main.append(Carte("trèfle", 5))
+        # self.joueur2.main.append(Carte("trèfle", 5))
+        print("Deck du joueur 1: ", self.joueur1.main)
+        print("Deck du joueur 2: ", self.joueur2.main)
 
-    def jouer_tour(self, forceClaim:bool = False) -> Joueur | None:
-        """ Bataille, bool -> Joueur | NoneType
-        Simule un tour de bataille """
-        print("Nouveau tour...")
-        if self.fin_partie(self.joueur1):
-            return self.joueur2
-        if self.fin_partie(self.joueur2):
-            return self.joueur1
-        self.pile.append(self.joueur1.jouer_carte())
-        self.pile.append(self.joueur2.jouer_carte())
-        print(self.pile)
-        # On compare les deux cartes
-        
-        if len(self.pile) == 2 or forceClaim:
-            bat = self.pile[-2].bat(self.pile[-1])
-            print(self.pile)
-            if bat is None:
-                return self.bataille()
-            if bat:
-                print("Le joueur 1 gagne !")
-                self.joueur1.ramasser(*self.pile)
-            else:
-                print("Le joueur 2 gagne !")
-                self.joueur2.ramasser(*self.pile)
-            self.pile = []
+    def load_deck(self, copied_deck:dict) -> bool:
+        """Bataille, dict -> bool
+        Charge la configuration du deck (copied_deck).
+        Renvoie True or False si la fonction se termine correctement.
+        """
+        if not copied_deck:
+            return False
+        cartes_j1 = copied_deck.get("joueur1")
+        cartes_j2 = copied_deck.get("joueur2")
 
+        self.joueur1.main = []
+        self.joueur2.main = []
+        for carte in cartes_j1:
+            self.joueur1.main.append(Carte(carte.get("couleur"), carte.get("nombre")))
 
-    def fin_partie(self, joueur = None):
-        """ Bataille, Joueur -> Joueur
-        Vérifie si la partie est terminée. Si c'est le cas, renvoie le joueur gagnant. """
-        if joueur:
-            return len(joueur.main) == 0
-        return len(self.joueur1.main) == 0 or len(self.joueur2.main) == 0
+        for carte in cartes_j2:
+            self.joueur2.main.append(Carte(carte.get("couleur"), carte.get("nombre")))
+        if len(self.joueur1.main) + len(self.joueur2.main) == 52:
+            return True
+        return Exception("Deck chargé Invalide. La somme des cartes des deux joueurs doit être égale à 52.")
+    
+    def save_deck(self) -> "Bataille":
+        """ Bataille -> Bataille
+        Sauvegarde le deck actuel. """
+        deck =  {
+            "joueur1": [{"couleur": carte.couleur, "nombre": carte.nombre} for carte in self.joueur1.main],
+            "joueur2": [{"couleur": carte.couleur, "nombre": carte.nombre} for carte in self.joueur2.main]
+        }
 
-    def bataille(self):
-        """ Bataille -> NoneType
-        Déclenche une bataille """
-        self.jouer_tour() # Juste append, vu qu'il y a déjà deux cartes.
-        winner = self.jouer_tour(forceClaim=True) # On joue une carte de plus pour déterminer le gagnant; on force la comparaison.
-        print(f"Bataille remportée par {winner}")
+        with open(f"{time.time()}-deck.json", "w", encoding="utf-8") as f:
+            json.dump(deck, f, ensure_ascii=False, indent=4)
+        return self
 
-    def run(self):
-        """ Bataille -> Joueur
-        Simule une bataille entre les deux joueurs """
-        self.distribuer()
-        while True:
-            if self.fin_partie():
-                break
+    def run(self) -> Joueur | None:
+        """ Bataille -> Joueur | NoneType
+        Lance une partie de bataille """
+        while not self.fin_partie():
+            # input("Prochain tour\n")
             self.jouer_tour()
-            
-        print("Partie terminée !")
+            print(f"CARTES JOUEUR 1 [{len(self.joueur1.main)}]", ": ", self.joueur1.main)
+            print(f"CARTES JOUEUR 2 [{len(self.joueur2.main)}]: ", self.joueur2.main)
+        return self.fin_partie(getPlayer=True)
