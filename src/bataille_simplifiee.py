0cad63205d1e28be19dd4dc98b7b22f6ac527ae8
Thibault Morez <contact@saturna19.fr>
2024-10-27 21:42:27 +0100

    Add simplified version of the card game (really fast)

---
+++ src/bataille_simplifiee.py
@@ -0,0 +1,7 @@
+from random import randint
+
+# Pourquoi se compliquer la vie quand c'est un jeu de hasard finalement ?
+if randint(0, 1) == 1:
+    print("Le Joueur 1 gagne")
+else:
+    print("Le Joueur 2 gagne")
